4975e1a0c562f4e60d15cb120ef9cff40ccc42cc
3 <karisam81@gmail.com>
2022-05-13 22:32:19 +0900

    .

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,29 +1,58 @@
 import './App.css';
 import { useState } from 'react';
 
+function Modal() {
+  return (
+    <div id="card">
+      <p style={{ fontWeight: "bolder" }}>모달 우동맛집</p>
+      <span>2월 17일 발행</span>
+    </div>
+  );
+};
+
+let i = 1;
+
 function App() {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
   const clickedToggle = () => {
-    setToggle((prev) => !prev);
+    setToggle(toggle => !toggle);
   };
 
+  let titles = ['파이썬 독학', '자바 스터디', '가나다라', '마바사아'];
+  console.log(titles);
+  function ascending(a, b) {
+    return (a < b) ? -1 : (a === b) > 0;
+  };
+  titles.sort(ascending);
+  console.log(titles);
+
+  const [title, setTitle] = useState('파이썬 독학');
+  const editTitle = () => {
+    let a = i % 4;
+    setTitle(titles[a]);
+    i++;
+  };
 
   return (
     <div className='container'>
       <div className='header'>ReactBlog</div>
       <div id="card">
-        <p style={{ fontWeight: "bolder" }}>{!toggle ? '남자' : '여자'}코트 추천</p>
+        <p style={{ fontWeight: "bolder" }}>{toggle ? '남자' : '여자'}코트 추천</p>
         <span>2월 17일 발행</span>
+        <br />
+        <button onClick={clickedToggle}>클릭</button>
       </div>
       <div id="card">
         <p style={{ fontWeight: "bolder" }}>강남 우동맛집</p>
         <span>2월 17일 발행</span>
       </div>
       <div id="card">
-        <p style={{ fontWeight: "bolder" }}>파이썬 독학</p>
+        <p style={{ fontWeight: "bolder" }}>{title}</p>
         <span>2월 17일 발행</span>
+        <br />
+        <button onClick={editTitle}>Edit</button>
       </div>
-      <button onClick={clickedToggle} toggle={toggle}>클릭</button>
+      <Modal />
     </div>
   );
 }
